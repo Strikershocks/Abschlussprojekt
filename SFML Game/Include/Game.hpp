a52a912ac0b5c1a5020ab29dde7ce79bbd5a3f1c
Strikershocks <n.krassmann@hotmail.de>
2015-05-27 19:07:34 +0200

    SpriteAnimation Test

--- SFML Game/Include/Game.hpp
+++ SFML Game/Include/Game.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include "AnimatedSprite.hpp"
 
 class Game : private sf::NonCopyable
 {
@@ -20,12 +21,23 @@ class Game : private sf::NonCopyable
 		static const float PlayerSpeed;
 		static const sf::Time TimePerFrame;
 
+		// Animation für Links
+		Animation walkingAnimationLeft;
+
+		// Animation für Rechts
+		Animation walkingAnimationRight;
+		Animation* currentAnimation;
+		// set up AnimatedSprite
+		AnimatedSprite animatedSprite();
+
 		sf::RenderWindow Window;
 		sf::Texture Texture;
 		sf::Sprite Player;
 		sf::Font Font;
 		sf::Text StatisticsText;
 		sf::Time StatisticsUpdateTime;
+		sf::Clock frameClock;
+		sf::Time frameTime;
 
 		std::size_t StatisticsNumFrames;
 		bool IsMovingUp;
